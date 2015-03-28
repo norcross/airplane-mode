c901b4a90622c34a115fccf62147a0a4408ec6fd
Chris Christoff <chriscct7@users.noreply.github.com>
2015-03-28 14:13:14 -0400

    Not sure how that got there

--- airplane-mode.php
+++ airplane-mode.php
@@ -18,7 +18,7 @@ the Free Software Foundation; version 2 of the License (GPL v2) only.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.ta
+GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
